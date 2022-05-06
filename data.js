46a0f05015aaf4d0d5d58a4c75c6493216c9e8a2
Sanju Kamat <sanjukamat123@gmail.com>
2022-05-06 09:54:16 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,131 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.3224445070751436,
+        "pitch": -0.07590875486658,
+        "fov": 0.9554825331534866
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2068368484325642,
+          "pitch": 0.013304558750721895,
+          "rotation": 0,
+          "target": "1-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.051711910186778454,
+        "pitch": -0.00001593867121130188,
+        "fov": 0.6851358915897463
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4129915455107742,
+          "pitch": 0.0498226305790439,
+          "rotation": 0,
+          "target": "0-1"
+        },
+        {
+          "yaw": -1.440440069451574,
+          "pitch": 0.04790644995335569,
+          "rotation": 0,
+          "target": "2-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-3",
+      "name": "3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.6778791644085835,
+        "pitch": -0.050037920139672565,
+        "fov": 0.6468310685968685
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1013661919776077,
+          "pitch": 0.014220563098875871,
+          "rotation": 0,
+          "target": "1-2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Casinos",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
